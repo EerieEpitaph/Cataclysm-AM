8fee7e56c45bba18a8f5e7031b5c6ceae6cd0d7f
Ottavio Lischio <ottavio.lischio42@gmail.com>
2018-04-12 12:43:42 +0200

    Hash underway, again

--- src/ItemDatabase.h
+++ src/ItemDatabase.h
@@ -14,7 +14,7 @@ class ItemDatabase
         ItemDatabase(std::unordered_map<uint32_t, Item&> dat) : database(dat){}
         inline std::unordered_map<uint32_t, Item&> getDatabase(){return database;}
 
-        void addItem(Item toAdd){database.insert( std::make_pair( FNVHash(toAdd.getName()), toAdd     ));}
+        void addItem(Item& toAdd){database.insert( std::make_pair( FNVHash(toAdd.getName()), toAdd     ));}
         Item& fetch(uint32_t hashd);
 };
 
